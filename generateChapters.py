1fe3a22daff7558ba72ded180f1642fed767d880
Samuel Walker <31997383+samuel-walker@users.noreply.github.com>
2018-04-16 15:51:45 -0700

    Expanding python script
    Expanding functionality for coop training manual creation

--- generateChapters.py
+++ generateChapters.py
@@ -20,20 +20,65 @@ with open('chapters.csv', 'r') as csvfile: # open csv
         if row[0] == "":
             pass
         else:
-            md = urllib.URLopener() # start url reader
-            #download md file
-            md.retrieve(bookpath + branch + row[7], row[8] + "_read.md")
-            md_read = open(row[8] + "_read.md","r") # open downloaded md file
+            # check if dir exists, make it if not
             if not os.path.exists(row[5]):
                 os.makedirs(row[5])
+            md = urllib.URLopener() # start url reader
+            # download md file
+            md.retrieve(bookpath + branch + row[7], row[8] + "_read.md")
+            md_read = open(row[8] + "_read.md","r") # open downloaded md file
             md_write = open(row[8],"w") # new temp write md file
             for line in md_read: # iterate on md file lines
-                # replace relative with absolute paths
+                # edit image paths
                 line = line.replace("](./", "](../" + row[7].split("/")[0] + "/")
                 md_write.write(line) # print new line in temp file
+            # identify and download required images
+            for line in md_read:
+                pat = line.find("](../")
+                img = pat.findall(md_read)
             md_write.close() # close temp file
             md_read.close() # close md file
             os.remove(row[8] + "_read.md")
+
+
+
+with open('chapters.csv', 'r') as csvfile: # open csv
+    next(csvfile, None) # skip header
+    data = csv.reader(csvfile, delimiter=',') # define csvreader
+    for row in data: # iterate on urls to generate chapters
+        if row[0] == "":
+            pass
+        else:
+            # check if dir exists, make it if not
+            if not os.path.exists(row[5]):
+                os.makedirs(row[5])
+            if not os.path.exists(row[5] + "/Images"):
+                os.makedirs(row[5] + "/Images")
+            md = urllib.URLopener() # start url reader
+            # download md file
+            md.retrieve(bookpath + branch + row[7], row[8] + "_read.md")
+            md_read = open(row[8] + "_read.md","r") # open downloaded md file
+            md_write = open(row[8],"w") # new temp write md file
+            for line in md_read: # iterate on md file lines
+                # edit image paths
+                line = line.replace("](./", "](../" + row[7].split("/")[0] + "/")
+                if "](../" in line:
+                    img = line[6:-2]
+                    # img = urllib.URLopener() # start url reader for img
+                    # download image
+                    urllib.urlretrieve(bookpath + branch + row[7] + img, img)
+                    # img_write = open(img,"w") # new temp write md file
+                    # img_write.write(line) # print new line in temp file
+                md_write.write(line) # print new line in temp file
+            # identify and download required images
+            # pat = re.compile("\]\(\.\.\/.*\)")
+            # for line in md_read:
+            #     img = pat.find(line)
+            # print img
+            md_write.close() # close temp file
+            md_read.close() # close md file
+            os.remove(row[8] + "_read.md")
+
 
 # generate summary.md to create book structure
 summary = open("SUMMARY.md","w") # open summary.md to write
@@ -46,4 +91,5 @@ with open('chapters.csv', 'r') as csvfile: # open csv
         title = re.sub("([a-z])([A-Z])","\g<1> \g<2>",row[6])
         # write link in summary with proper indentation
         summary.write(2*int(row[3])*" " + "* [" + title[5:-3] + "](" + row[8] + ")\n")
+
 summary.close() # close summary.md
